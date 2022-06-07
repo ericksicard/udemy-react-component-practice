53ae157946e23e2e1b773ce1ecae978d110dfc7c
ericksicard <sicard.erick@gmail.com>
2022-06-07 18:56:09 -0400

    Adding User List Component and Managing it via State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import {useState} from 'react';
 import AddUser from './components/Users/AddUsers';
+import UsersList from './components/Users/UsersList';
+
+
 
 
 function App() {
+
+  const [userList, setUserList] = useState([])
+
+  const addUserHandler = (userName, userAge) => {
+    setUserList( prevState => {
+      return [...prevState ,{name: userName, age: userAge, id: Math.random().toString()}];
+  })};
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={userList}/>
     </div>
   );
 }
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -17,6 +17,7 @@ const AddUser = props => {
         if (+enteredAge < 1) {
             return;
         }
+        props.onAddUser(enteredUserName, enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
     };
